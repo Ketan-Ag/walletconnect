4a2d95dc70b7f37f2a37faf1d490f1a91031109f
ketan-fernglasz <ketan.agarwal@fernglasz.com>
2024-03-11 17:19:46 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
+import Connect from './Connect';
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = process.env.PROJECT_ID
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+
+// 3. Create modal
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: process.env.URL, // origin must match your domain & subdomain
+  icons: [process.env.ICONS]
+}
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet],
+  projectId,
+})
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Connect />
     </div>
   );
 }
---
+++ src/Connect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Connect = () => {
+  return (
+    <div>
+      <w3m-button/>
+    </div>
+  )
+}
+
+export default Connect
